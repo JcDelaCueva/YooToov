dc5906eb86d11424017825e7d51a53db26a2a2ff
JcDelaCueva <delacuevajc28@gmail.com>
2017-08-07 13:27:30 +0800

    Update Constants.java

--- app/src/main/java/com/jcdelacueva/yootoov/Constants.java
+++ app/src/main/java/com/jcdelacueva/yootoov/Constants.java
@@ -5,9 +5,9 @@ package com.jcdelacueva.yootoov;
  */
 
 public class Constants {
-    public static final String SERVER_URL = "https://yootoov-server.glitch.me/yootoov";
-    public static final String APP_ID = "535099c3c122172393f628db1601fb1a";
-    public static final String API_KEY = "AIzaSyBt3ltiFMXRh13_F8wzICFoRKW_KjQUWGM";
+    public static final String SERVER_URL = "SERVER_URL";
+    public static final String APP_ID = "APP_ID";
+    public static final String API_KEY = "API_KEY";
     public static final String y2mp3ApiHost = "www.youtubeinmp3.com";
     public static final String y2mp3ApiSegment = "fetch";
     public static final String y2mp3Api = "www.youtubeinmp3.com/fetch/?format=JSON&video=%s";
